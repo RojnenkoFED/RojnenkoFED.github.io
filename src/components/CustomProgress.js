37c440b62db8c46c5c2956073c10cd4c20a6a55e
Rozhnenko Mikhail <mrozhnenko@hapicloud.io>
2025-03-09 14:02:52 +0300

    added custom element "custom progress"

---
+++ src/components/CustomProgress.js
@@ -0,0 +1,126 @@
+class CustomProgress extends HTMLElement {
+
+    static get attributesMap() {
+        return {
+            animated: {
+                isBoolean: true,
+                setValue(isAnimated) {
+                    if (isAnimated) {
+                        this.style.animation = '3s linear infinite rotate';
+                    } else {
+                        this.style.animationPlayState = 'paused';
+                    }
+                }
+            },
+            hidden : {
+                isBoolean: true,
+                setValue(isHidden) {
+                    this.style.display = isHidden ? 'none' : 'grid';
+                }
+            },
+            value: {
+                setValue(progressValue) {
+                    if (progressValue) {
+                        this.style.setProperty('--progress-value', `${progressValue}%`);
+                    }
+                }
+            },
+        }
+    };
+
+    static get observedAttributes() {
+        return Object.keys(this.attributesMap);
+    }
+
+    _styleCSS = `
+        :host {
+            --size: 7.5rem;
+            --bar-width: 0.75rem;
+
+            --fill-color: #005dff;
+            --rest-color: #eef3f6;
+        }
+    
+        .progress {
+            --donat-mask: radial-gradient(
+                farthest-side,
+                transparent calc(100% - var(--bar-width)),
+                #fff calc(100% - var(--bar-width) + 1px)
+            );
+
+            display: grid;
+            place-items: center;
+            width: var(--size);
+            aspect-ratio: 1 / 1;
+            border-radius: 50%;
+            background: conic-gradient(
+                var(--fill-color) var(--progress-value, 0),
+                var(--rest-color) var(--progress-value, 0)
+            );
+            -webkit-mask: var(--donat-mask);
+                    mask: var(--donat-mask);
+        }
+            
+        @keyframes rotate {
+            from {
+                transform: rotate(0deg);
+            }
+
+            to {
+                transform: rotate(360deg);
+            }
+        }
+    `;
+
+    constructor() {
+        super();
+    }
+
+    setCustomAttribute(name, params) {
+        const progressEl = this.shadowRoot?.querySelector('.progress');
+
+        if (progressEl) {
+            const value = new CustomAttribute(name, params?.isBoolean).getValue(this);
+            params.setValue?.call(progressEl, value);
+        }
+    }
+
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        const progressEl = document.createElement('div');
+        progressEl.setAttribute('class', 'progress');
+
+        const style = document.createElement('style');
+        style.textContent = this._styleCSS;
+
+        shadow.appendChild(style);
+        shadow.appendChild(progressEl);
+
+        if (CustomProgress.observedAttributes.length) {
+            for (const [name, params] of Object.entries(CustomProgress.attributesMap)) {
+                this.setCustomAttribute(name, params);
+            }
+        }
+    }
+
+    attributeChangedCallback(name) {
+        this.setCustomAttribute(name, CustomProgress.attributesMap[name]);
+    }
+}
+
+class CustomAttribute {
+    name;
+    isBoolean;
+
+    constructor(name, isBoolean = false) {
+        this.name = name;
+        this.isBoolean = isBoolean;
+    }
+
+    getValue(element) {
+        return this.isBoolean ? element.hasAttribute(this.name) : element.getAttribute(this.name);
+    }
+}
+
+customElements.define('custom-progress', CustomProgress);
